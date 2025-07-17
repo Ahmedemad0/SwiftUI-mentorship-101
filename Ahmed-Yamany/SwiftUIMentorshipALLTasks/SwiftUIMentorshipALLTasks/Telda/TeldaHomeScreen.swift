91ab56b968609098f17990e0f75b4927f89db61b
Ahmed Yamany <yamany2862016@gmail.com>
2025-07-18 00:29:47 +0300

    end telda home screen

--- Ahmed-Yamany/SwiftUIMentorshipALLTasks/SwiftUIMentorshipALLTasks/Telda/TeldaHomeScreen.swift
+++ Ahmed-Yamany/SwiftUIMentorshipALLTasks/SwiftUIMentorshipALLTasks/Telda/TeldaHomeScreen.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+// MARK: - i focus on requiremensts not details ^_^
+// here i focused on scroll offset tracking and animating views
 struct TeldaHomeScreen: View {
     @State private var scrollOffset: CGRect = .zero
 
@@ -50,10 +52,12 @@ struct TeldaHomeScreen: View {
                     scrollOffset = $0
                 }
 
-                VStack {
+                VStack(spacing: 24) {
                     ballanceView
                     actionButtons
+                    shopSection
                 }
+                .padding(.horizontal, 12)
             }
         }
         .coordinateSpace(name: "TeldaHomeScreen")
@@ -83,11 +87,10 @@ struct TeldaHomeScreen: View {
         }
         .frame(height: 200)
     }
-    
+
     private var actionButtons: some View {
         HStack {
             Button {
-                
             } label: {
                 Label("Add Money", systemImage: "plus")
                     .font(.headline.bold())
@@ -95,25 +98,57 @@ struct TeldaHomeScreen: View {
                     .padding(.vertical, 8)
                     .background(.white.opacity(0.2), in: .capsule)
             }
-            
+
             Button {
-                
             } label: {
                 Image(systemName: "wallet.bifold")
                     .frame(width: 40, height: 40)
                     .background(.white.opacity(0.2), in: .circle)
             }
-            
+
             Button {
-                
             } label: {
                 Image(systemName: "ellipsis")
                     .rotationEffect(.degrees(90))
                     .frame(width: 40, height: 40)
                     .background(.white.opacity(0.2), in: .circle)
             }
+        }
+    }
 
+    private var shopSection: some View {
+        VStack(alignment: .leading) {
+            Text("Shop with discounts")
 
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack {
+                    ForEach(0 ..< 5) { _ in
+                        ZStack(alignment: .bottom) {
+                            Color.orange
+
+                            VStack(alignment: .leading) {
+                                Spacer()
+                                Text("noon Marketplaces")
+                                    .font(.footnote.weight(.medium))
+                            }
+                            .overlay(alignment: .topLeading) {
+                                Image("test")
+                                    .frame(width: 25, height: 25)
+                                    .background(.white)
+                                    .clipShape(.circle)
+                                    .offset(y: -25/2)
+                            }
+                            .padding([.bottom], 8)
+                            .padding(.horizontal, 4)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 80)
+                            .background(.black.opacity(0.3))
+                        }
+                        .frame(width: 120, height: 180)
+                        .clipShape(.rect(cornerRadius: 16))
+                    }
+                }
+            }
         }
     }
 }
